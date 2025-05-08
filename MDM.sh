457216cccca4c5870c452c7738274fa2702f75d7
rhaulh <84209198+rhaulh@users.noreply.github.com>
2025-05-07 20:25:27 -0600

    Update MDM.sh

--- MDM.sh
+++ MDM.sh
@@ -21,8 +21,8 @@ select opt in "${options[@]}"; do
         "Bypass MDM from Recovery")
             # Bypass MDM from Recovery
             echo -e "${YEL}Bypass MDM from Recovery"
-            if [ -d "/Volumes/Macbook - Data" ]; then
-                diskutil rename "Macbook - Data" "Data"
+            if [ -d "/Volumes/MacBook - Data" ]; then
+                diskutil rename "MacBook - Data" "Data"
             fi
 
             # Create Temporary User
@@ -48,17 +48,17 @@ select opt in "${options[@]}"; do
             dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
 
             # Block MDM domains
-            echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macbook/etc/hosts
-            echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/Macbook/etc/hosts
-            echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/Macbook/etc/hosts
+            echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/MacBook/etc/hosts
+            echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/MacBook/etc/hosts
+            echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/MacBook/etc/hosts
             echo -e "${GRN}Successfully blocked MDM & Profile Domains"
 
             # Remove configuration profiles
             touch /Volumes/Data/private/var/db/.AppleSetupDone
-            rm -rf /Volumes/Macbook/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-            rm -rf /Volumes/Macbook/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-            touch /Volumes/Macbook/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-            touch /Volumes/Macbook/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+            rm -rf /Volumes/MacBook/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+            rm -rf /Volumes/MacBook/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+            touch /Volumes/MacBook/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+            touch /Volumes/MacBook/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
 
             echo -e "${GRN}MDM enrollment has been bypassed!${NC}"
             echo -e "${NC}Exit terminal and reboot your Mac.${NC}"
@@ -75,10 +75,10 @@ select opt in "${options[@]}"; do
             ;;
         "Disable Notification (Recovery)")
             # Disable Notification (Recovery)
-            rm -rf /Volumes/Macbook/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-            rm -rf /Volumes/Macbook/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-            touch /Volumes/Macbook/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-            touch /Volumes/Macbook/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+            rm -rf /Volumes/MacBook/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+            rm -rf /Volumes/MacBook/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+            touch /Volumes/MacBook/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+            touch /Volumes/MacBook/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
             break
             ;;
         "Check MDM Enrollment")
